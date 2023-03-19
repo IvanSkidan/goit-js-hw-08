13034ba88d975577831fea4cc7b415254caaf260
Ivan Skidan <vanyalife7@gmail.com>
2023-03-19 16:21:24 +0200

    completed one task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li>
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}"/>
+  </a>
+  </li>`
+}).join('');
+
+galleryContainer.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
