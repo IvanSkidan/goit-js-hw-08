b5ded19561b0ce458918af8c9a0ee2e64e9c7b1b
Ivan Skidan <vanyalife7@gmail.com>
2023-03-21 12:01:02 +0200

    completed all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const onPlay = function (event) {
+  localStorage.setItem(TIME_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle  from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form  input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+form.addEventListener('input', event => {
+  // console.log(event.target.name);
+  // console.log(event.target.value);
+  
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+});
+
+populateForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  // console.log('Відправляємо форму');
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+function onFormInput(event) {
+  // const message = event.target.value;
+  // console.log(message);
+  formData = {
+    email: input.value,
+    message: textarea.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function populateForm() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!savedMessage) {
+    return;
+  };
+
+  input.value = savedMessage.email || '';
+  textarea.value = savedMessage.message || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.message || '';
+};
